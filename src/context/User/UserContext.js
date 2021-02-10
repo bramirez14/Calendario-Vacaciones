9529ecbb9121f1588a8c17a5566fe14aaf342365
Bramirez <bramirezsm14@gmail.com>
2021-02-09 23:30:52 -0300

    iniciando el calendario

---
+++ src/context/User/UserContext.js
@@ -0,0 +1,6 @@
+//El createContext transforma el contenido en un estado y no solo en u componente
+import { createContext } from "react";
+
+const UserContext = createContext();
+
+export default UserContext;
